6ec66d7fc018a97dc32e6aff702fa9cd2076c302
rezakazazy8 <rezakazazy8@yahoo.com>
2023-12-27 13:05:22 +0330

    ConstantMultiplicationMethod

---
+++ Servies/ConstantMultiplicationMethod.cs
@@ -0,0 +1,41 @@
+﻿namespace random.Servies
+{
+    public class ConstantMultiplicationMethod
+    {
+        private string _k;
+        private string _x;
+        private int n;
+        public ConstantMultiplicationMethod(string k, string x)
+        {
+
+            _k = k;
+            _x = x;
+            this.n = k.Length;
+        }
+
+        public decimal Next()
+        {
+            decimal k = decimal.Parse(_k);
+            decimal x = decimal.Parse(_x);
+
+            decimal product = k * x;
+
+            var sString = product.ToString().PadLeft(n * 2, '0');
+
+            var startIndex = n / 2;
+            var endIndex = sString.Length - n / 2;
+
+            var seedString = sString.Substring(startIndex, endIndex - startIndex);
+
+            decimal result;
+            if (!decimal.TryParse("0." + seedString, out result))
+            {
+                result = 0.0m;
+            }
+
+            _x = seedString;
+
+            return result;
+        }
+    }
+}
