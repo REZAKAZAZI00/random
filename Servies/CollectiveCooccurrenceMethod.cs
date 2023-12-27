5435115454bfc5b89d230fc4faff36431b6dfe18
rezakazazy8 <rezakazazy8@yahoo.com>
2023-12-27 23:26:57 +0330

    CollectiveCooccurrenceMethod

---
+++ Servies/CollectiveCooccurrenceMethod.cs
@@ -0,0 +1,27 @@
+﻿namespace random.Servies
+{
+    public class CollectiveCooccurrenceMethod
+    {
+        private int[] sequence;
+        private int currentIndex;
+
+        public CollectiveCooccurrenceMethod(int[] x, int n, int m)
+        {
+            sequence = new int[x.Length + n];
+            Array.Copy(x, sequence, n);
+            currentIndex = n;
+
+            for (int i = n; i < x.Length + n; i++)
+            {
+                sequence[i] = (sequence[i - 1] + sequence[i - n]) % m;
+            }
+        }
+
+        public int Next()
+        {
+            int result = sequence[currentIndex];
+            currentIndex = (currentIndex + 1) % sequence.Length;
+            return result;
+        }
+    }
+}
